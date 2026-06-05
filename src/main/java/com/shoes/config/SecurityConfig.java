abb1cc61c720c8ed48e91c4b58fc6f7672cdc53a
decade <thonglaanh1011@gmail.com>
2026-06-05 23:04:49 +0700

    first commit

--- src/main/java/com/shoes/config/SecurityConfig.java
+++ src/main/java/com/shoes/config/SecurityConfig.java
@@ -33,7 +33,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5174",
-                "https://shoes-web.onrender.com", "https://shoes-web-1.onrender.com"));
+                "https://shoes-web.onrender.com", "https://shoes-web-1.onrender.com",
+                "https://shoes-web-client-one.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(false);
